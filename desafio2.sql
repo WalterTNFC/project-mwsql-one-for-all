0fbf288e9ad91b7dc0f4cb92009e34e04be4e46e
WalterTNFC <walter.tad@hotmail.com>
2022-03-14 14:13:39 -0300

    Refatorando requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,5 @@
 -- Referencia: https://stackoverflow.com/questions/43300665/sql-how-to-combine-count-results-from-multiple-tables-into-multiple-columns
-USE SpotifyClone;
 SELECT
-	(SELECT COUNT(sons_id) FROM sons) AS 'cancoes',
-    (SELECT COUNT(artistas_id) FROM artistas) AS 'artistas',
-    (SELECT COUNT(album_id) FROM albuns) AS 'albuns';
+	(SELECT COUNT(sons_id) FROM SpotifyClone.sons) AS 'cancoes',
+    (SELECT COUNT(artistas_id) FROM SpotifyClone.artistas) AS 'artistas',
+    (SELECT COUNT(album_id) FROM SpotifyClone.albuns) AS 'albuns';
